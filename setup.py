c829978d0dfd483845adf9185593744c47d2e082
zachary-hamilton <57552986+zachary-hamilton@users.noreply.github.com>
2020-05-07 00:36:50 -0400

    preparing to release to pypi

---
+++ setup.py
@@ -0,0 +1,20 @@
+# setup.py file
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="lambdata-zachary-hamilton", # the name that you will install via pip
+    version="1.0",
+    author="Zachary Hamilton",
+    author_email="your@email.com",
+    description="A short description",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    #license="MIT",
+    url="https://github.com/zachary-hamilton/lambdata-zachary-hamilton",
+    #keywords="",
+    packages=find_packages() # ["my_lambdata"]
+)
