fc8f420c0cbd341c4c28f07f2c4bdd266e52d94c
zachary-hamilton <57552986+zachary-hamilton@users.noreply.github.com>
2020-05-10 21:32:02 -0400

    commenting the code

--- lambdata_zachary_hamilton/lambdata_zachary_hamilton.py
+++ lambdata_zachary_hamilton/lambdata_zachary_hamilton.py
@@ -7,12 +7,16 @@ def list_to_column(df, lst, column_name):
     This function takes a list and adds it as a new column of
     a dataframe
     '''
+
+    # turns the original list into a series
     my_series = pd.Series(lst)
 
+    # adding the series to the dataframe
+    df = pd.concat([df, my_series], axis=1)
+
+    # renaming the column headers
     columns = df.columns.tolist()
     columns.append(column_name)
-
-    df = pd.concat([df, my_series], axis=1)
     df.columns = columns
     return df
 
@@ -24,22 +28,24 @@ def split_dates(df, column, year=True, month=True,
     comoponent parts
     '''
 
+    df = df.copy()
+
+    # turning the values in the columns into datetime format
     df[column] = df[column].apply(
         lambda x: pd.to_datetime(
-            x, infer_datetime_format=True))
+            x, infer_datetime_format=True))     
 
+    # these if statements display the designated date data
     if year:
         df['year'] = df[column].apply(lambda x: x.year)
-
     if month:
         df['month'] = df[column].apply(lambda x: x.month)
-
     if day:
         df['day'] = df[column].apply(lambda x: x.day)
-
     if weekday:
         df['weekday'] = df[column].apply(lambda x: x.weekday())
 
+    # this will remove the original column if desired
     if replace:
         df = df.drop(columns=[column])
 
