0fde82c741aeb5101a1b4b7b464c69ccdd98f947
zachary-hamilton <57552986+zachary-hamilton@users.noreply.github.com>
2020-05-10 21:16:33 -0400

    post autopep8

--- lambdata_zachary_hamilton/lambdata_zachary_hamilton.py
+++ lambdata_zachary_hamilton/lambdata_zachary_hamilton.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+
 def list_to_column(df, lst, column_name):
     '''
     This function takes a list and adds it as a new column of
@@ -10,19 +11,23 @@ def list_to_column(df, lst, column_name):
 
     columns = df.columns.tolist()
     columns.append(column_name)
-        
+
     df = pd.concat([df, my_series], axis=1)
     df.columns = columns
     return df
 
-def split_dates(df, column, year=True, month=True, day=True, weekday=True, replace=True):
+
+def split_dates(df, column, year=True, month=True,
+                day=True, weekday=True, replace=True):
     '''
     This function will take a dataframe column that is a date and split it into its
     comoponent parts
     '''
 
-    df[column] = df[column].apply(lambda x: pd.to_datetime(x, infer_datetime_format=True))
-    
+    df[column] = df[column].apply(
+        lambda x: pd.to_datetime(
+            x, infer_datetime_format=True))
+
     if year:
         df['year'] = df[column].apply(lambda x: x.year)
 
@@ -34,7 +39,7 @@ def split_dates(df, column, year=True, month=True, day=True, weekday=True, repla
 
     if weekday:
         df['weekday'] = df[column].apply(lambda x: x.weekday())
-    
+
     if replace:
         df = df.drop(columns=[column])
 
