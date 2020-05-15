db0cfdc8ad69c4ff8b1727b982b8cfd114eb1958
zachary-hamilton <57552986+zachary-hamilton@users.noreply.github.com>
2020-05-14 22:07:56 -0400

    third test

--- test/test.py
+++ test/test.py
@@ -34,6 +34,20 @@ class TestWranglingFunctions(unittest.TestCase):
         self.assertEqual(col.add_to_dataframe(df).iloc[0][0], 1)
         self.assertEqual(col.add_to_dataframe(df).iloc[0][1], '2020-5-6')
 
+    def test_split_dates(self):
+        df = pd.DataFrame({'a':[1,2,3], 'dates':['2020-5-6', '2020-12-25', '1990-03-10']})
+        split_df = split_dates(df, 'dates')
+        
+        # checking columns
+        self.assertEqual(split_df.columns[0], 'a')
+        self.assertEqual(split_df.columns[1], 'year')
+        self.assertEqual(split_df.columns[2], 'month')
+        self.assertEqual(split_df.columns[-1], 'weekday')
+
+        # checking a value
+        self.assertEqual(split_df.iloc[0][1], 2020)
+        self.assertEqual(split_df.iloc[0][2], 5)
+
 
 if __name__ == '__main__':
     unittest.main()
