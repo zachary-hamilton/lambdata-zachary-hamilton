43b8be16c0d85b2d21c9acdad1786fdf8035150c
zachary-hamilton <57552986+zachary-hamilton@users.noreply.github.com>
2020-05-14 21:58:53 -0400

    second unit test

--- test/test.py
+++ test/test.py
@@ -18,8 +18,21 @@ class TestWranglingFunctions(unittest.TestCase):
         self.assertEqual(df.columns[0], 'a')
         self.assertEqual(df.columns[1], 'dates')
 
+        # testing that the values were added correctly
         self.assertEqual(df.iloc[0][0], 1)
         self.assertEqual(df.iloc[0][1], '2020-5-6')
+
+    def test_Column(self):
+        df = pd.DataFrame({'a':[1,2,3]})
+        lst = ['2020-5-6', '2020-12-25']
+        col = Column(lst, 'dates')
+        
+        # checking that the class worked
+        self.assertEqual(col.my_series[0], '2020-5-6')
+
+        # checking the method
+        self.assertEqual(col.add_to_dataframe(df).iloc[0][0], 1)
+        self.assertEqual(col.add_to_dataframe(df).iloc[0][1], '2020-5-6')
 
 
 if __name__ == '__main__':
